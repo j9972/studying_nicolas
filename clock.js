2b769e9fe96efabd5fca861cea31c6a1b30d752f
jung su young <j9972@naver.com>
2021-03-03 00:28:53 +0900

    getTime 함수를 실행을 했는데 현재 시간에 대해서 메인 화면에 나오지가 않음

--- clock.js
+++ clock.js
@@ -1,8 +1,16 @@
 const clockContainer = document.querySelector(".js-clock");
 const clockTitle = clockContainer.querySelector("h1");
 
-function init() {
+function getTime() {
+    const date = new Date();
+    const minutes = date.minutes();
+    const hours = date.getHours();   
+    const seconds = date.getSeconds();
+    clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
+}
 
+function init() {
+    getTime();
 };
 
 init();
