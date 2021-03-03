549deddeaf2a6c0ecddcbf40511ea299a41ab510
jung su young <j9972@naver.com>
2021-03-04 00:13:15 +0900

    showing 부분의 display를 none으로 해놔서 text가 보이지 않았었던 에러 수정

---
+++ gretting.js
@@ -0,0 +1,28 @@
+const form = document.querySelector('.js-form');
+const input = form.querySelector('input');
+const greeting = document.querySelector('.js-greetings');
+
+const USER_LS = "currentUser";
+const SHOWING_CN = "showing";
+
+function paintGreeting(text) {
+    form.classList.remove(SHOWING_CN);
+    greeting.classList.add(SHOWING_CN);
+    greeting.innerText = `Hello ${text}`;
+}
+
+//localstroage 기능을 하는 함수
+function loadName() {
+    const currentUser = localStorage.getItem(USER_LS);
+    if(currentUser === null) { 
+        
+    } else { // => means if user exist in the localStorage then what happen
+        paintGreeting(currentUser)
+    }
+}
+
+function init() {
+    loadName();
+}
+
+init();
