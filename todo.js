045187d312d88c180b17ca099c3db7c3914e6492
jung su young <j9972@naver.com>
2021-03-04 14:37:19 +0900

    taking images and make bg.js

--- todo.js
+++ todo.js
@@ -4,17 +4,47 @@ const toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = 'toDos';
 
+let toDos = [];
+
+function deleteToDo(event) {
+    // target을 통해 어떤것을 눌렀는지 알 수 있게끔 해준다. parentnode로 그 요소의 부모가 누구인지도 알수있음
+    //console.log(event.target.parentNode);
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li); 
+    const cleanToDos = toDos.filter(function(toDo) {
+        return toDo.id !== parseInt(li.id);
+    });
+    toDos = cleanToDos;
+    saveToDos();
+}
+
+function saveToDos() {
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos)); 
+    //JSON.stringify은 obj를 string으로 바꿔준다.
+
+}
 
 function paintToDo(text) {
     //li 이라는 태그 생성
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
-    delBtn.innerText = "❌";
     const span = document.createElement("span");
+    // newId 로 const로 생성한 li의 id갑을 자동적으로 주어진다.
+    const newId = toDos.length + 1;
+    delBtn.innerText = "❌";
+    delBtn.addEventListener('click', deleteToDo);
     span.innerText = text;
-    li.appendChild(span);
     li.appendChild(delBtn);
+    li.appendChild(span);
+    li.id = newId;
     toDoList.appendChild(li);
+    const toDoObj = {
+        text: text,
+        id: newId
+    };
+    toDos.push(toDoObj);
+    saveToDos();
 }
 
 function handleSubmit(event) {
@@ -25,9 +55,12 @@ function handleSubmit(event) {
 }
 
 function loadToDos() {
-    const toDos = localStorage.getItem(TODOS_LS);
-    if(toDos !== null) {
-
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    if(loadedToDos !== null) {
+        const parsedToDos = JSON.parse(loadedToDos); // parse는 obj로 만들어 준다.
+        parsedToDos.forEach(function(toDo) {
+            paintToDo(toDo.text);
+        });
     }
 }
 
