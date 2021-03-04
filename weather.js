4023259a47e4a27ed32c6acbc28041f70668b5fa
jung su young <j9972@naver.com>
2021-03-04 15:37:20 +0900

    api key추가

--- weather.js
+++ weather.js
@@ -1,12 +1,31 @@
+const API_KEY = "4ef2746363982f1e4f139dbebde6059e";
 const COORDS = 'coords';
 
+function saveCoords(coordsObj) {
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSuccess(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+        latitude,
+        longitude
+    };
+    saveCoords(coordsObj);
+}
+
+function handleGeoError() {
+    console.log("cant access geo location");
+}
+
 function askForCoords() {
-    navigator
+    navigator.geolocation.getCurrentPosition(handleGeoSuccess,handleGeoError);
 }
 
 function loadCoords() {
     const loadedCords = localStorage.getItem(COORDS);
-    if(loadCoords === null) {
+    if(loadedCords === null) {
         askForCoords();
     } else {
 
@@ -14,7 +33,7 @@ function loadCoords() {
 }
 
 function init() {
-
+    loadCoords();
 }
 
 init();
