a433e18dc688f92134b3986e5b86ef7d68ab8d6c
jung su young <j9972@naver.com>
2021-03-09 00:30:47 +0900

    i use es6 syntax

--- react-movie-app/movie_app/src/App.js
+++ react-movie-app/movie_app/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import PropTypes from "prop-types"
 
 class App extends React.Component {
@@ -6,14 +7,22 @@ class App extends React.Component {
     isLoading: true, 
     movies: []
   };
+
+  // axios는 느리기 때문에 비동기로 만들어줄 필ㅇ가 있다. async -> await
+  getMovies = async() => {
+    const { // es6 syntax
+      data: { // 이렇게 나타내는건 console을 찍어보고 어느 지점에서 movie data가 있는지 확인하고 한것
+        data: {movies}
+      }
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    //console.log(movies); 
+    this.setState({ movies, isLoading : false });
+  }
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 6000); // 6초 후에 loading -> we are ready로 바뀐다. 
-    // api로부터 얻은 data를 fetch한것이다.
+    this.getMovies();
   }
   render() {
-    const {isLoading} = this.state; // es6 문법
+    const { isLoading } = this.state; // es6 문법
     return <div>{isLoading ? "Loading..." : "we are ready"}</div>;
   }
 }
