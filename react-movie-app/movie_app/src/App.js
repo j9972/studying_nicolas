fc6f7e6c51a2869c75067b9d2a3b08bc25faadb0
jung su young <j9972@naver.com>
2021-03-08 23:34:24 +0900

    starting react-movie-app component

--- react-movie-app/movie_app/src/App.js
+++ react-movie-app/movie_app/src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import PropTypes from "prop-types"
 
-function App() {
-  return (
-    <div>
-      hello
-    </div>
-    );
+class App extends React.Component {
+  state = {
+    isLoading: true, 
+    movies: []
+  };
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000); // 6초 후에 loading -> we are ready로 바뀐다. 
+    // api로부터 얻은 data를 fetch한것이다.
+  }
+  render() {
+    const {isLoading} = this.state; // es6 문법
+    return <div>{isLoading ? "Loading..." : "we are ready"}</div>;
+  }
 }
 
 export default App;
+
