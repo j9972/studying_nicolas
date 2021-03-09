64503115b5ccd3c952f785ecc3edcf81dbb6b538
jung su young <j9972@naver.com>
2021-03-09 23:03:53 +0900

    styling and add genre

--- react-movie-app/movie_app/src/Movie.js
+++ react-movie-app/movie_app/src/Movie.js
@@ -3,14 +3,19 @@ import PropTypes from "prop-types";
 import "./Movie.css";
 
 // 인자를 { } 로 감싸지 않으면 화면에 표시가 안됨
-function Movie({ year, title, summary, poster}) {
-    return <div class="movie">
-        <img src={poster} alt={title} title={title} />
-        <div class="movie__data">
-            <h3 class="movie__title">{title}</h3>
-            <h5 class="movie__year">{year}</h5>
-            <p class="movie__summary">{summary}</p>
+function Movie({ year, title, summary, poster, genres}) {
+    return <div className="movie">
+    <img src={poster} alt={title} title={title} />
+        <div className="movie__data">
+            <h3 className="movie__title">{title}</h3>
+            <h5 className="movie__year">{year}</h5>
+            <ul className="genres"> 
+                {genres.map((genre, index) => ( 
+                    <li key={index} className="genres__genre">{genre}</li>
+                ))} 
+            </ul>
         </div>
+            <p className="movie__summary">{summary}</p>
     </div>
 }
 
@@ -20,6 +25,7 @@ Movie.propTypes = {
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
     poster: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 }
 
 export default Movie;
