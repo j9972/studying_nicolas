0c0607259a0ceb6d4bcb244cc1cfe128cdc9819d
jung su young <j9972@naver.com>
2021-03-09 22:30:56 +0900

    i put movies' title, poster, year..etc...on my localhost

--- react-movie-app/movie_app/src/App.js
+++ react-movie-app/movie_app/src/App.js
@@ -24,20 +24,27 @@ class App extends React.Component {
   render() {
     const { isLoading, movies } = this.state; // es6 문법
     return (
-      <div>
-        {isLoading 
-          ? "Loading..." 
-          : movies.map(movie => ( 
-            <Movie 
-              key={movie.id}  
-              id={movie.id} 
-              year={movie.year} 
-              title={movie.title} 
-              summary={movie.summary} 
-              poster={movie.medium_cover_image} 
-            />        
-          ))}
-      </div>
+      <section class="container">
+        {isLoading ? (
+          <div class="loader">
+            <span class="loader__text">Loading... </span>
+          </div>
+          ) : (
+            <div class="movies">
+              {            
+              movies.map(movie => ( 
+                <Movie 
+                  key={movie.id}  
+                  id={movie.id} 
+                  year={movie.year} 
+                  title={movie.title} 
+                  summary={movie.summary} 
+                  poster={movie.medium_cover_image} 
+                />        
+              ))}
+            </div>
+          )}
+      </section>
     );
   }
 }
--- react-movie-app/movie_app/src/Movie.js
+++ react-movie-app/movie_app/src/Movie.js
@@ -2,8 +2,15 @@ import React from 'react';
 import PropTypes from "prop-types";
 
 // 인자를 { } 로 감싸지 않으면 화면에 표시가 안됨
-function Movie({id, year, title, summary, poster}) {
-    return <h4>{title}</h4>
+function Movie({ year, title, summary, poster}) {
+    return <div class="movie">
+        <img src={poster} alt={title} title={title} />
+        <div class="movie__data">
+            <h3 class="movie__title">{title}</h3>
+            <h5 class="movie__year">{year}</h5>
+            <p class="movie__summary">{summary}</p>
+        </div>
+    </div>
 }
 
 Movie.propTypes = {
