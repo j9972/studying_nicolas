1cdf94704f1b1fc8c26500a36a4b259fb0a47e03
jung su young <j9972@naver.com>
2021-03-10 21:19:21 +0900

    Finish

--- react-movie-app/movie_app/src/App.js
+++ react-movie-app/movie_app/src/App.js
@@ -9,7 +9,7 @@ import "./App.css";
 function App() {
   return <HashRouter>
     <Navigation />
-    <Route path="/" exact={true} component={Home}/>
+    <Route path="/" exact={true} component={Home}/> 
     <Route path="/about" component={About}/>
     <Route path="/movie/:id" component={Detail}/>
   </HashRouter>
