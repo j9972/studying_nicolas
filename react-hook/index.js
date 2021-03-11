b0b28be888d9a9d08768f32a4a70885909f1adbf
jung su young <j9972@naver.com>
2021-03-11 17:44:28 +0900

    i practice useTabs for useState by react-hook

--- react-hook/index.js
+++ react-hook/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-import "./style.css";
+// import "./style.css";
 
 const content = [
     {
@@ -14,16 +14,34 @@ const content = [
     }
 ]
 
+const useTabs = (initialTab, allTabs) => {
+    //에러 확인용 -> 아래 if문은 array가 아닐때 return을 햐준다.
+    if(!allTabs || Array.isArray(allTabs)) {
+        return;
+    }
+    const [currentIndex, setCurrentIndex] = useState(initialTab);
+    return {
+        currentItem: allTabs[currentIndex],
+        changeItem: setCurrentIndex
+    };
+}
+
 const App = () => {
     // const [item, setItem] = useState(0);
     // const IncrementItem = () => setItem(item + 1);
     // const DecrementItem = () => setItem(item - 1);
+    const {currentItem} = useTabs(0, content);
     return (
         <div className="App">
             {/* 위 코드는 <h1>Hello</h1> {item} 이 원래 코드 */}
             {/* <button onClick={IncrementItem}>Increment</button>
             <button onClick={DecrementItem}>Decrement</button> */}
-            {content.map(section => (<button>{section.tab}</button>))}
+            {content.map((section, index) => (
+                <button onClick={() => changeItem(index)}>{section.tab}</button>
+            ))}
+            <div>
+            {currentItem.content}
+            </div>
         </div>
     );
 };
