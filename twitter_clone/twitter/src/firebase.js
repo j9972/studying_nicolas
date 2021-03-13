268067a6f95e0cb875b8a5edfd392a1306af5e4e
jung su young <j9972@naver.com>
2021-03-13 23:30:56 +0900

    making smth

---
+++ twitter_clone/twitter/src/firebase.js
@@ -0,0 +1,21 @@
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+    appId: process.env.REACT_APP_APP_ID,
+};
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyBLyhke55iBfY4TFajdoSzZaCK7D8ZX_vs",
+//     authDomain: "nwitter-7e49f.firebaseapp.com",
+//     projectId: "nwitter-7e49f",
+//     storageBucket: "nwitter-7e49f.appspot.com",
+//     messagingSenderId: "465951404655",
+//     appId: "1:465951404655:web:e3728237d08c3a3f5d139a"
+//   };
+
+export default firebase.initializeApp(firebaseConfig);
