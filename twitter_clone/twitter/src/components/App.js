d8013b181346f566adeb00aebac24934056b32a0
jung su young <j9972@naver.com>
2021-03-15 00:15:49 +0900

    에러 수정중

--- twitter_clone/twitter/src/components/App.js
+++ twitter_clone/twitter/src/components/App.js
@@ -4,11 +4,12 @@ import {authService} from "fbase";
 
 function App() {
   //user가 있는지 확인하는 코드 authService.currentUser
+  console.log(authService.currentUser);
   const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
   return (
     <>
       <AppRouter isLoggedIn={isLoggedIn}/>;
-      <footer>&copy; Nwitter {new Date().getFullYear()}</footer>
+      <footer>&copy; { new Date().getFullYear() } Nwitter </footer>
     </>
   )
 }
--- twitter_clone/twitter/src/routes/Auth.js
+++ twitter_clone/twitter/src/routes/Auth.js
@@ -1,4 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Auth = () => <span>Auth</span>
+const Auth = () => {
+    const [email, setEmail] = useState();
+    return (
+        <div>
+            <form>
+                <input type="text" placeholder="Email" required />
+                <input type="password" placeholder="Password" required />
+                <input type="submit" placeholder="Log In" required />
+            </form>
+            <div>
+                <button>Continue with Google</button>
+                <button>Continue with Github</button>
+            </div>
+        </div>
+    );
+}
+
 export default Auth;
