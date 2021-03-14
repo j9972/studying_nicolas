51be5a7283234a2541387a06859aae1fe0dfaa24
jung su young <j9972@naver.com>
2021-03-15 01:08:19 +0900

    firebase가 안깔려 있던거

--- twitter_clone/twitter/src/components/App.js
+++ twitter_clone/twitter/src/components/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import AppRouter from 'components/Router';
-import {authService} from "fbase";
+import {authService} from 'fbase';
 
 function App() {
   //user가 있는지 확인하는 코드 authService.currentUser
@@ -8,7 +8,7 @@ function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
   return (
     <>
-      <AppRouter isLoggedIn={isLoggedIn}/>;
+      <AppRouter isLoggedIn={isLoggedIn}/>
       <footer>&copy; { new Date().getFullYear() } Nwitter </footer>
     </>
   )
--- twitter_clone/twitter/src/routes/Auth.js
+++ twitter_clone/twitter/src/routes/Auth.js
@@ -2,12 +2,25 @@ import React, { useState } from "react";
 
 const Auth = () => {
     const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const onChange = (event) => {
+        const {target: {name, value}} = event;
+        if(name === "email") {
+            setEmail(value);
+        } else if( name === "password") {
+            setPassword(value);
+        }
+        console.log(event.target.name);
+    }
+    const onSubmit = (event) => {
+        event.preventDefault();
+    }
     return (
         <div>
-            <form>
-                <input type="text" placeholder="Email" required />
-                <input type="password" placeholder="Password" required />
-                <input type="submit" placeholder="Log In" required />
+            <form onSubmit={onSubmit}>
+                <input name="email" type="text" placeholder="Email" required value={email} onChange={onChange} />
+                <input name="password" type="password" placeholder="Password" required value={password} onChange={onChange}/>
+                <input type="submit" pvalue="Log In"/>
             </form>
             <div>
                 <button>Continue with Google</button>
