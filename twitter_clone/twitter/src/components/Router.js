2ead1938c238308852350ac2b5483f11e0d588bd
jung su young <j9972@naver.com>
2021-03-14 22:50:33 +0900

    Router의 상수를 app 파일로 옮김

--- twitter_clone/twitter/src/components/Router.js
+++ twitter_clone/twitter/src/components/Router.js
@@ -3,9 +3,8 @@ import {HashRouter as Router, Route, Switch} from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
 
-const AppRouter = () => {
+const AppRouter = ({isLoggedIn}) => {
     // useState의 값이 false면 auth고 true면 home page가 나타나게 된다
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
     return (
         <Router>
             <Switch>
