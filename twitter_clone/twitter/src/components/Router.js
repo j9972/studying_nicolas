b03731d144813338f9dba7432dc0a092641b3428
jung su young <j9972@naver.com>
2021-03-16 01:29:05 +0900

    Log out by redirection

--- twitter_clone/twitter/src/components/Router.js
+++ twitter_clone/twitter/src/components/Router.js
@@ -1,23 +1,33 @@
 import React, { useState } from "react";
-import {HashRouter as Router, Route, Switch} from "react-router-dom";
-import Auth from "../routes/Auth";
-import Home from "../routes/Home";
+import {HashRouter as Router, Redirect, Route, Switch} from "react-router-dom";
+import Auth from "routes/Auth";
+import Home from "routes/Home";
+import Profile from "routes/Profile";
+import Navigation from "components/Navigation";
 
 const AppRouter = ({isLoggedIn}) => {
     // useState의 값이 false면 auth고 true면 home page가 나타나게 된다
     return (
         <Router>
+            {isLoggedIn && <Navigation />}
             <Switch>
                 {isLoggedIn ? (
                     <>
                     <Route exact path="/">
                         <Home />
                     </Route>
+                    <Route exact path="/profile">
+                        <Profile />
+                    </Route>
+                    <Redirect from="*" to="/" />
                     </> 
                     ) : ( 
-                    <Route exact path="/">
-                        <Auth />
-                    </Route> 
+                    <>
+                        <Route exact path="/">
+                            <Auth />
+                        </Route> 
+                        <Redirect from="*" to="/" />
+                    </>
                 )}
             </Switch>
         </Router>
