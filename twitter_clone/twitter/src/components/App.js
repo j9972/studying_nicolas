962bb1027627849967a57f751f072d12cb84b1c1
jung su young <j9972@naver.com>
2021-03-16 00:17:59 +0900

    account 즉 유저를 만들고 로그인을 했는데 화면에서 계속 로그인 창이 보이는 이유는 해결중

--- twitter_clone/twitter/src/components/App.js
+++ twitter_clone/twitter/src/components/App.js
@@ -1,11 +1,19 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import AppRouter from 'components/Router';
 import {authService} from 'fbase';
 
+//user가 있는지 확인하는 코드 authService.currentUser
 function App() {
-  //user가 있는지 확인하는 코드 authService.currentUser
+  const [init, setInit] = useState(false);
   console.log(authService.currentUser);
   const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  useEffect(() => {
+    
+  }, []);
+  // console.log(authService.currentUser)
+  // setInterval(() => {
+  //   console.log(authService.currentUser);
+  // }, 2000);
   return (
     <>
       <AppRouter isLoggedIn={isLoggedIn}/>
