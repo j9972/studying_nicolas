831758fbeb6b3cc790944975027ce94536c6af55
jung su young <j9972@naver.com>
2021-03-16 22:47:10 +0900

    get 메소드로 데이터의 정보를 볼수있음

--- twitter_clone/twitter/src/routes/Home.js
+++ twitter_clone/twitter/src/routes/Home.js
@@ -1,8 +1,16 @@
 import { dbService } from "fbase";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
     const [nweet, setNweet] = useState("");
+    const [nweets, setNweets] = useState([]);
+    const getNNweets = async() => {
+        const dbNweets = await dbService.collection("nweets").get();
+        dbNweets.forEach((document) => console.log(document.data()));
+    }
+    useEffect(() => {
+        getNNweets();
+    }, []);
     const onSubmit = async (event) => {
         event.preventDefault();
         await dbService.collection("nweets").add({
