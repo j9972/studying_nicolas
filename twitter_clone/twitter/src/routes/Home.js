8668538125362623cf293e60475d1ef71845ee4e
jung su young <j9972@naver.com>
2021-03-19 23:25:44 +0900

    Home js file에 있는 코드 정리

--- twitter_clone/twitter/src/routes/Home.js
+++ twitter_clone/twitter/src/routes/Home.js
@@ -1,12 +1,10 @@
 import { dbService, storageService } from "fbase";
-import {v4 as uuidv4} from "uuid";
 import React, { useEffect, useState } from "react";
 import Nweet from 'components/Nweet';
+import NweetFactory from "components/NweetFactory";
 
 const Home = ({userObj}) => {
-    const [nweet, setNweet] = useState("");
     const [nweets, setNweets] = useState([]);
-    const [attachment, setAttachment] = useState("");
     const getNNweets = async() => {   
         const dbNweets = await dbService.collection("nweets").get();
         // 이 부분이 realTime
@@ -25,54 +23,10 @@ const Home = ({userObj}) => {
             setNweets(nweetArray);
         })
     }, []);
-    const onSubmit = async (event) => {
-        event.preventDefault();
-        let attachmentUrl = "";
-        // collection이랑  ref.child랑 비슷
-        if(attachment !== "") {
-            const attachmentRef = storageService.ref().child(`${userObj.uid}/${uuidv4()}`);
-            const response = await attachmentRef.putString(attachment, "data_url");
-            attachmentUrl = await response.ref.getDownloadURL();
-        }    
-        const nweetObj = {
-            text:nweet, // state의 nweet랑 같은 거니까 이렇게 둬도 된다
-            createdAt: Date.now(),
-            creatorId: userObj.uid,
-            attachmentUrl,
-        };
-        
-        await dbService.collection("nweets").add(nweetObj);
-        setNweet("");
-        setAttachment("");
-    };
-    const onChange = (event) => {
-        const {target:{value}} = event;
-        setNweet(value);
-    };
-    const onFileChange = (event) => {
-        const {target:{files}}= event;
-        const theFile = files[0];
-        const reader = new FileReader();
-        reader.onloadend = (finishedEvent) => {
-            const {currentTarget: {result}} = finishedEvent;
-            setAttachment(result);
-        };
-        reader.readAsDataURL(theFile);
-    };
-    const onCleaerAttachment = () => setAttachment(null);
+    
     return (
         <div>
-            <form onSubmit={onSubmit}>
-                <input value={nweet} onChange={onChange} type="text" placeholder="whats on ur mind?" maxLength={120} />
-                <input type="file" accept="image/*" onChange={onFileChange}/>
-                <input type="submit" value="Nweet" />
-                {attachment && (
-                    <div>
-                        <img src={attachment} width="50px" height="50px" />
-                        <button onClick={onCleaerAttachment}>Clear</button>
-                    </div>
-                    )}
-            </form>
+            <NweetFactory userObj={userObj}/>
             <div>
                 {nweets.map((nweet) => (
                     <Nweet key={nweet.id} nweetObj={nweet} isOwner={nweet.creatorId === userObj.uid}/>
